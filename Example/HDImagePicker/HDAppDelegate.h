ed15fb6c204e49f3604df3c087ecbab158f7b411
HarshitDaftary <daftaryharshit@yahoo.co.in>
2014-09-04 16:01:26 +0530

    Initial commit

---
+++ Example/HDImagePicker/HDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HDAppDelegate.h
+//  HDImagePicker
+//
+//  Created by CocoaPods on 09/04/2014.
+//  Copyright (c) 2014 HarshitDaftary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
